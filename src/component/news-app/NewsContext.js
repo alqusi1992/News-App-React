4c557f434cde5f2082043fa8127364abba8132e1
alqusi1992 <35297889+alqusi1992@users.noreply.github.com>
2021-09-21 18:37:36 +0200

    Update NewsContext.js

--- src/component/news-app/NewsContext.js
+++ src/component/news-app/NewsContext.js
@@ -6,18 +6,18 @@ export const NewsContextProvider = (props) => {
   const [data, setData] = useState([]);
   const [category, setCategory] = useState("general");
 
-  // const API_KEY = process.env.REACT_NEWS_APP_API_KEY;
+  const API_KEY = process.env.REACT_NEWS_APP_API_KEY;
 
   const value = {
     data,
     setCategory,
     category,
   };
-  const API = "9c3733b2527641a4aac4447732331e4a";
+
   const fetchNewsData = async () => {
     try {
       const response = await fetch(
-        `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API}&category=${category}`
+        `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}&category=${category}`
       );
       const requestData = await response.json();
       setData(requestData.articles);
